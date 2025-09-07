56e5d070778e1287b8dcc0e65408b432c20b0933
senira277 <senira277@gmail.com>
2025-09-07 09:24:29 +0530

    initial commit

---
+++ src/main/java/com/swiftTrack/ros_mobile_backend/RosMobileBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swiftTrack.ros_mobile_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosMobileBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosMobileBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swiftTrack/ros_mobile_backend/RosMobileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftTrack.ros_mobile_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosMobileBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
